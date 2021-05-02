d71c3cb8aee5e1b328e7b305f22ae19021a90688
JLWolff <1200049@isep.ipp.pt>
2021-05-02 20:38:54 +0100

    adding javadoc for testtype and testtypestore

--- src/main/java/app/domain/model/TestType.java
+++ src/main/java/app/domain/model/TestType.java
@@ -7,15 +7,43 @@ import java.util.List;
 
 public class TestType {
 
+    /**
+     * Test Type unique code
+     */
     private String code;
+    /**
+     * Test Type description
+     */
     private String description;
+    /**
+     * Test Type collecting method
+     */
     private String collectingMethod;
+    /**
+     * List of Categories assigned to test type
+     */
     private List<ParameterCategory> selectedCategories;
 
+    /**
+     * Max lenght of description field
+     */
     private static final int DESCRIPTION_MAXLENGTH = 15;
+    /**
+     * Max lenght of code field
+     */
     private static final int CODE_MAXLENGTH = 5;
+    /**
+     * Max lenght of collecting method field
+     */
     private static final int COLLECTING_METHOD_MAXLENGTH = 20;
 
+    /**
+     * Full constructor of test type
+     * @param code
+     * @param description
+     * @param collectingMethod
+     * @param selectedCategories
+     */
     public TestType(String code, String description, String collectingMethod, List<ParameterCategory> selectedCategories) {
         checkCode(code);
         checkDescription(description);
@@ -30,6 +58,10 @@ public class TestType {
         return code;
     }
 
+    /**
+     * Code attribute validation for having non alphanumeric characters, more or less then 5 characters or blank
+     * @param code
+     */
     private void checkCode(String code){
         if (StringUtils.isBlank(code))
             throw new IllegalArgumentException("Code cannot be blank.");
@@ -39,6 +71,10 @@ public class TestType {
             throw new IllegalArgumentException("Code must only have alphanumeric characters.");
     }
 
+    /**
+     * Decription attribute validation for having more then 15 characters or being blank
+     * @param description
+     */
     private void checkDescription(String description){
         if (StringUtils.isBlank(description))
             throw new IllegalArgumentException("Description cannot be blank.");
@@ -46,6 +82,10 @@ public class TestType {
             throw new IllegalArgumentException("Description cannot have more than 15 characters.");
     }
 
+    /**
+     * Collecting method attribute validation for having more then 20 characters or being blank
+     * @param atributte
+     */
     private void checkCollectingMethod(String atributte){
         if (StringUtils.isBlank(atributte))
             throw new IllegalArgumentException("Collecting method cannot be blank.");
