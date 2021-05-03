121389628fe6f1026704cae9263dd3c1f503101f
JLWolff <1200049@isep.ipp.pt>
2021-05-03 17:57:01 +0100

    adding dto functions to controller

---
+++ src/main/java/app/mappers/CategoriesMapper.java
@@ -0,0 +1,25 @@
+package app.mappers;
+
+import app.mappers.dto.CategoriesDTO;
+import app.domain.model.ParameterCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesMapper {
+
+    public CategoriesDTO toDTO(ParameterCategory category)
+    {
+        return new CategoriesDTO(category.getName(),category.getCode());
+    }
+
+    public List<CategoriesDTO> toDTO(List<ParameterCategory> categories)
+    {
+        List<CategoriesDTO> categoriesDTO = new ArrayList<>();
+        for(ParameterCategory category : categories)
+        {
+            categoriesDTO.add(this.toDTO(category));
+        }
+        return categoriesDTO;
+    }
+}
