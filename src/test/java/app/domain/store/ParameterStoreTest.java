8eda209c38ffb3708c070b6ab5053d84ce1c0c45
martaribeiro <1201592@isep.ipp.pt>
2021-05-04 20:08:37 +0100

    Continued US10 Tests.

---
+++ src/test/java/app/domain/store/ParameterStoreTest.java
@@ -0,0 +1,18 @@
+package app.domain.store;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParameterStoreTest {
+
+    @Test
+    public void createsParameterStore() {
+        ParameterStore ps1 = new ParameterStore();
+
+    }
+
+
+
+}
