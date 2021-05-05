9124c9d95ee395db417666b71f4cd7a0e176320d
martaribeiro <1201592@isep.ipp.pt>
2021-05-05 19:05:20 +0100

    Started US10 JavaDoc Documentation.

--- src/main/java/app/domain/model/Parameter.java
+++ src/main/java/app/domain/model/Parameter.java
@@ -4,13 +4,45 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
+/**
+ * Represents a Parameter through:
+ * a code, a name, a description and a parameter category.
+ *
+ * @author Marta Ribeiro 1201592
+ */
+
 public class Parameter {
 
+    /**
+     * The parameter code.
+     */
     private String parameterCode;
+
+    /**
+     * The parameter name.
+     */
     private String shortName;
+
+    /**
+     * The parameter description.
+     */
     private String description;
+
+    /**
+     * The parameter category of the parameter.
+     */
     private ParameterCategory pc; //Parameter is presented under 1 ParameterCategory
 
+    /**
+     *
+     * Builds a parameter instance receiving:
+     * the parameter code, name, description and parameter category.
+     *
+     * @param parameterCode the parameter code.
+     * @param shortName the parameter name.
+     * @param description the parameter description.
+     * @param pc the parameter category of the parameter.
+     */
     public Parameter(String parameterCode, String shortName, String description, ParameterCategory pc){
         checkParameterCodeRules(parameterCode);
         checkShortNameRules(shortName);
@@ -25,6 +57,19 @@ public class Parameter {
         return pc;
     }*/
 
+    /**
+     * Returns true if the parameter code received in the parameter respects
+     * all the rules.
+     * It returns false as soon as one of these conditions are not verified
+     * by the parameter code provided:
+     * - It is blank.
+     * - It has a length different than 5.
+     *
+     * @param parameterCode the parameter code.
+     *
+     * @return true if the parameter code respects all the rules,
+     * otherwise returns false.
+     */
     private void checkParameterCodeRules(String parameterCode){
         if (StringUtils.isBlank(parameterCode))
             throw new IllegalArgumentException("Code cannot be blank.");
@@ -32,6 +77,19 @@ public class Parameter {
             throw new IllegalArgumentException("Code must have 5 chars.");
     }
 
+    /**
+     * Returns true if the parameter name received in the parameter respects
+     * all the rules.
+     * It returns false as soon as one of these conditions are not verified
+     * by the parameter name provided:
+     * - It is blank.
+     * - It has a length longer than 8.
+     *
+     * @param shortName the parameter name.
+     *
+     * @return true if the parameter name respects all the rules,
+     * otherwise returns false.
+     */
     private void checkShortNameRules(String shortName){
         if (StringUtils.isBlank(shortName))
             throw new IllegalArgumentException("Name cannot be blank.");
@@ -39,6 +97,19 @@ public class Parameter {
             throw new IllegalArgumentException("Name must have up to 8 chars.");
     }
 
+    /**
+     * Returns true if the parameter description received in the parameter respects
+     * all the rules.
+     * It returns false as soon as one of these conditions are not verified
+     * by the parameter description provided:
+     * - It is blank.
+     * - It has a length longer than 20.
+     *
+     * @param description the parameter description.
+     *
+     * @return true if the parameter description respects all the rules,
+     * otherwise returns false.
+     */
     private void checkDescriptionRules(String description){
         if (StringUtils.isBlank(description))
             throw new IllegalArgumentException("Description cannot be blank.");
