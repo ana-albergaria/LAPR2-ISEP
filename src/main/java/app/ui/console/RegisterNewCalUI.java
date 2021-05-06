9dd5b1b7d82cc740794fc2c1f8b82961b99392d0
Ana Albergaria <1201518@isep.ipp.pt>
2021-05-06 12:08:37 +0100

    Doing US8 UI

--- src/main/java/app/ui/console/RegisterNewCalUI.java
+++ src/main/java/app/ui/console/RegisterNewCalUI.java
@@ -17,43 +17,65 @@ public class RegisterNewCalUI implements Runnable {
     }
 
     public void run() {
+        boolean success;
 
-
+        System.out.print("To register a new Clinical Analysis Laboratory, please insert the requested data.");
+        do {
+            success = registerClinicalAnalysisLaboratory();
+        } while (!success);
+        System.out.println("\nClinical Analysis Laboratory successfully created!");
 
 
     }
+
     private boolean registerClinicalAnalysisLaboratory() {
         boolean invalidData = true;
-        System.out.printf("To register a new Clinical Analysis Laboratory, please insert the requested data.");
-
-        do {
-            try {
-                String laboratoryID = Utils.readLineFromConsole("Laboratory ID: ");
-                String name = Utils.readLineFromConsole("Name: ");
-                String address = Utils.readLineFromConsole("Address: ");
-                String phoneNumber = Utils.readLineFromConsole("Phone Number: ");
-                String numTIN = Utils.readLineFromConsole("TIN Number: ");
-                List<String> selectedTT = getTypesOfTestToCode();
-
-                ClinicalAnalysisLaboratoryDTO calDto = new ClinicalAnalysisLaboratoryDTO(laboratoryID,
-                        name, address, phoneNumber, numTIN, selectedTT);
-
-                ctrl.createClinicalAnalysisLaboratory(calDto);
-
-                boolean confirm = Utils.confirm("Please, confirm the data (type `s` if its correct, `n` if it is not):");
 
 
+        try {
 
+            ClinicalAnalysisLaboratoryDTO calDto = insertingData();
+
+            ctrl.createClinicalAnalysisLaboratory(calDto);
+
+            boolean confirm = Utils.confirm(String.format("Please, confirm the data (type `s` if its correct, `n` if it is not):" +
+                            "%nLaboratory ID: %s%nName: %s%nAddress: %s%nPhone Number: %s%nTIN Number: %s%nTypes Of Test: %s%n",
+                    calDto.getLaboratoryID(), calDto.getName(), calDto.getAddress(),
+                    calDto.getPhoneNumber(), calDto.getNumTIN(), calDto.getTestTypeCodes()));
+
+            if (!confirm) {
+                System.out.printf("Please, insert again the data you wish.");
+                calDto = insertingData();
+            } else {
+                boolean save = ctrl.saveClinicalAnalysisLaboratory();
+                if (save) {
+                    invalidData = false;
+
+                } else {
+                    throw new IllegalArgumentException("That Clinical Analysis Laboratory is already registered in the system.");
+                }
 
             }
-            catch(IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        } while(invalidData);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
 
-
+        return invalidData;
     }
+
+    private ClinicalAnalysisLaboratoryDTO insertingData() {
+        String laboratoryID = Utils.readLineFromConsole("Laboratory ID: ");
+        String name = Utils.readLineFromConsole("Name: ");
+        String address = Utils.readLineFromConsole("Address: ");
+        String phoneNumber = Utils.readLineFromConsole("Phone Number: ");
+        String numTIN = Utils.readLineFromConsole("TIN Number: ");
+        List<String> selectedTT = getTypesOfTestToCode();
+
+        return new ClinicalAnalysisLaboratoryDTO(laboratoryID,
+                name, address, phoneNumber, numTIN, selectedTT);
+    }
+
     private List<TestTypeDTO> showTestTypeListAndSelectingThem(List<TestTypeDTO> listTestType) {
         List<TestTypeDTO> listTestTypeDto = ctrl.getTestTypes();
         TestTypeDTO testTypeToAdd = (TestTypeDTO) Utils.showAndSelectOne(listTestTypeDto,
@@ -69,7 +91,10 @@ public class RegisterNewCalUI implements Runnable {
         listTestType = showTestTypeListAndSelectingThem(listTestType);
         List<String> testTypesCodes = new ArrayList<>();
         for (TestTypeDTO tt : listTestType) {
-            testTypesCodes.add(tt.getCode());
+            if(tt == null)
+                throw new IllegalArgumentException("You should select at least one type of test in order to register a new Clinical Analysis Laboratory!");
+            else
+                testTypesCodes.add(tt.getCode());
         }
         return testTypesCodes;
     }
