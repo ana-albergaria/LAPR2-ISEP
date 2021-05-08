3e92dde1032cf2c1d2951c1f899d22c8b656c8f1
martaribeiro <1201592@isep.ipp.pt>
2021-05-08 01:14:12 +0100

    Continued US7 Tests + Removed some useless imports.

--- src/main/java/app/domain/model/Company.java
+++ src/main/java/app/domain/model/Company.java
@@ -12,7 +12,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  *
---
+++ src/test/java/app/domain/model/OrgRoleTest.java
@@ -0,0 +1,29 @@
+package app.domain.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrgRoleTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureDescriptionNotEmpty(){
+        OrgRole instance = new OrgRole("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureDescriptionNotNull(){
+        OrgRole instance = new OrgRole(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureDescriptionNoLongerThan15Chars(){
+        OrgRole instance = new OrgRole("The Person Who Talks To The Clients");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureDescriptionHasOnlyLetters(){
+        OrgRole instance = new OrgRole("R3c3pc10n1st");
+    }
+
+}
