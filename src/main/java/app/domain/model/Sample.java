620ae362aaecc81d11ee8f0fb50575e5ec1a9d08
Ana Albergaria <1201518@isep.ipp.pt>
2021-05-20 20:30:14 +0100

    US5 Code Continuation

--- src/main/java/app/domain/model/Sample.java
+++ src/main/java/app/domain/model/Sample.java
@@ -1,21 +1,38 @@
 package app.domain.model;
 
 import app.domain.shared.ExternalAPI;
+import net.sourceforge.barbecue.BarcodeException;
 
 public class Sample {
     private Barcode barcode;
 
-    /*
+
     public Sample() {
-        this.barcode = getBarcode();
+        //this.barcode = getBarcode();
     }
 
-    public String getExternalAPI() throws IllegalAccessException, InstantiationException {
-        Class<?> oClass = Class.forName(“app.domain.shared.ExternalAPI”);
+    public ExternalAPI getExternalAPI() throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        //no parâmetro do Class.forName, vai ser colocada a String retirada da leitura do ficheiro de configuração
+        Class<?> oClass = Class.forName("app.domain.shared.BarbecueAdapter");
 
-        ExternalAPI extApi = (ExternalAPI) oClass.newInstance();
+        return (ExternalAPI) oClass.newInstance();
+    }
+
+    /*
+    public Barcode getBarcode() throws IllegalAccessException, ClassNotFoundException, InstantiationException, BarcodeException {
+        ExternalAPI api = getExternalAPI();
+
+        String barcodeNumber = "";
+
+        net.sourceforge.barbecue.Barcode barcode = api.getBarcode(barcodeNumber);
+
+        return barcode;
 
     }
-    
+
      */
+
+
+
+
 }
