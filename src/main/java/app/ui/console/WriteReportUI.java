c8a505adc4dda4ab0de91bd449da93d65308741e
martaribeiro <1201592@isep.ipp.pt>
2021-05-29 01:27:32 +0100

    Corrected US14 UI.

--- src/main/java/app/ui/console/WriteReportUI.java
+++ src/main/java/app/ui/console/WriteReportUI.java
@@ -26,14 +26,14 @@ public class WriteReportUI implements Runnable {
             System.out.println("To make a new diagnosis and write a report, please insert the requested data.");
             do {
                 int index = Utils.showAndSelectIndex(menu, ""); //shows list of tests ready to diagnose and asks to select one
-                success = (index == -1) ? true : createReport();
+                success = (index == -1) ? true : createAndAddReport();
             } while (!success);
             confirm = Utils.confirm("Do you intend to make more diagnosis and write more reports?\n[Type 's' for yes or 'n' for no.]");
         } while (confirm);
-        System.out.println("Reports successfully created!");
+        System.out.println("Reports successfully made!");
     }
 
-    private boolean createReport() {
+    private boolean createAndAddReport() {
         boolean confirmation;
         boolean success;
         try {
@@ -50,15 +50,19 @@ public class WriteReportUI implements Runnable {
 
             String reportText = Utils.readLineFromConsole("Write the report bellow.\n[you can use SHIFT + ENTER for paragraphs]");
 
-
             ctrl.createReport(reportText);
+            //ctrl.addReportToTest(tst.getCode());
             confirmation = Utils.confirm(String.format(">> REPORT <<" +
                             "%n%s" +
                             "%n[Type 's' for correct or 'n' for wrong.]",
                     reportText));
             if (!confirmation) throw new Exception("Please enter the correct data.");
+
             success = ctrl.createReport(reportText);
             if (!success) throw new Exception("Report is null. Please try again.");
+
+            ctrl.addReportToTest(tst.getCode());
+            
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
             success = false;
@@ -71,8 +75,7 @@ public class WriteReportUI implements Runnable {
 
     private TestDTO showListAndSelectOneObject() {
         List<TestDTO> testDTO = ctrl.getTestsToDiagnose();
-        TestDTO test = (TestDTO) Utils.showAndSelectOne(testDTO, "Enter the number of the test for which you want to make a diagnosis and write a report: ");
-        return test;
+        return (TestDTO) Utils.showAndSelectOne(testDTO, "Enter the number of the test for which you want to make a diagnosis and write a report: ");
     }
 
 }
