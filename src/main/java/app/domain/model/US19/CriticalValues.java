f35588b1eacb028a0b868374f502196829fc108a
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-01 23:22:00 +0100

    Adding Code Already given to Us in Moodle for US19 and US18

---
+++ src/main/java/app/domain/model/US19/CriticalValues.java
@@ -0,0 +1,35 @@
+package app.domain.model.US19;
+/*
+import org.apache.commons.math3.distribution.FDistribution;
+import org.apache.commons.math3.distribution.TDistribution;
+
+public class CriticalValues {
+    public class Main {
+
+
+        public static void main(String[] args) {
+
+
+
+            // Table number 7 from MATCP - t-Student
+            TDistribution td= new TDistribution(2);
+            double alphaTD =0.25;
+            if(alphaTD> 0.5) {
+                double critTD = td.inverseCumulativeProbability(alphaTD);
+                System.out.println("t-student critical value: " + critTD);
+            }
+            else {
+                double critTD = td.inverseCumulativeProbability(1 - alphaTD);
+                System.out.println("t-student critical value: " + critTD);
+            }
+
+            // Table number 8 from MATCP - Fisher–Snedecor distribution
+            FDistribution fd= new FDistribution(3,4);
+            double alphaFD= 0.01;
+            double critFD= fd.inverseCumulativeProbability(1- alphaFD);
+            System.out.println("Fisher–Snedecor critical value:" + critFD);
+
+        }
+    }
+}
+ */
