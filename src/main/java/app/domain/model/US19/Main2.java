19cb1ca62f07a83484d3592569dc611211a5076c
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-06 19:44:56 +0100

    US19 Commenting error code

--- src/main/java/app/domain/model/US19/Main2.java
+++ src/main/java/app/domain/model/US19/Main2.java
@@ -5,9 +5,12 @@ import app.domain.model.Test;
 
 public class Main2 {
     public static void main(String[] args) {
+        /*
         Client c1 = new Client("1234567890123456","1234567890",d1,"1234567890","carlos@gmail.com","Carlos","12345678901");
         this.company.getClientStore().saveClient(c1);
         Test test1 = new Test("123456789012",c1,t1,listParameter);
         this.company.getTestStore().saveTest(test1);
+        
+         */
     }
 }
