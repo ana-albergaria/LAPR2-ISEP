ad61dd8c4bfdb08916e98a5ab892bce74d98dabc
martaribeiro <1201592@isep.ipp.pt>
2021-06-08 01:50:55 +0100

    Continued US16 Code.

--- src/main/java/app/controller/CompanyPerformanceAnalysisController.java
+++ src/main/java/app/controller/CompanyPerformanceAnalysisController.java
@@ -1,12 +1,10 @@
 package app.controller;
 
 import app.domain.model.Company;
-import app.domain.model.Test;
 import app.domain.store.ClientStore;
 import app.domain.store.TestStore;
-
-//import com.isep.mdis.Sum;
-import org.apache.commons.math3.stat.descriptive.summary.Sum;
+import com.isep.mdis.Sum;
+import org.apache.commons.lang3.time.DateUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -69,30 +67,45 @@ public class CompanyPerformanceAnalysisController {
     //WORKING DAY IS FROM 8:00 TO 20:00
     //SUNDAY IS NOT A WORKING DAY
 
-    public int findNumberOfTestsProcessedBetweenTwoDates(Date beginningOfHalfHour, Date endOfHalfHour){
-        //getNumberOfTestsByIntervalDateOfTestRegistration(beginningDate, endDate)
-        return 0;
+    //test store:
+    //getNumberOfTestsByIntervalDateOfTestRegistration(beginningDate, endDate)
+    //getNumberOfTestsByIntervalDateOfDiagnosis(beginningDate, endDate)
+
+    //eg.:
+    //firstDayToAnalise d1d1/m1m1/y1y1 às 08:00:00:00
+    //lastDay d2d2/m2m2/y2y2 às 19:59:59:59
+    public int[] makeIntervalArray(Date firstDayToAnalyse, Date lastDayToAnalyse){ //começa às 8:00 e acaba às 20:00
+        TestStore testStore = company.getTestStore();
+        ArrayList<Integer> intervalArrayList = new ArrayList<>();
+        int numRegistered;
+        int numValidated;
+        int intToKeep;
+        int minToAdd = 30;
+        Date date1 = firstDayToAnalyse;
+        Date date2 = DateUtils.addMinutes(date1, minToAdd);
+        do{
+            numRegistered = testStore.getNumberOfTestsByIntervalDateOfTestRegistration(date1, date2);
+            numValidated = testStore.getNumberOfTestsByIntervalDateOfDiagnosis(date1, date2);
+            date1 = DateUtils.addMinutes(date1, minToAdd);
+            date2 = DateUtils.addMinutes(date2, minToAdd);
+            intToKeep = numRegistered-numValidated;
+            intervalArrayList.add(intToKeep);
+        } while (date2.before(lastDayToAnalyse));
+        int[] intervalArray = new int[intervalArrayList.size()];
+        for (int i = 0; i < intervalArray.length; i++) {
+            intervalArray[i] = intervalArrayList.get(i).intValue();
+        }
+        return intervalArray;
     }
 
-    public int findNumberOfTestsWithResultsObtainedBetweenTwoDates(Date beginningOfHalfHour, Date endOfHalfHour){
-        //getNumberOfTestsByIntervalDateOfDiagnosis(beginningDate, endDate)
-        return 0;
-    }
-
-    public int[] makeIntervalArray(Date firstDayToAnalyse, Date lastDayToAnalyse){
-        //TO DO
-
-        return null;
-    }
-/*
-    public int[] findWorstSubIntWithBenchmarkAlgorithm(int[] interval){
+    public int[] findWorstSubIntWithBenchmarkAlgorithm(Date firstDayToAnalyse, Date lastDayToAnalyse){
+        int[] interval = makeIntervalArray(firstDayToAnalyse, lastDayToAnalyse);
         int[] worstSubInt = Sum.Max(interval);
         return worstSubInt;
     }
 
- */
-
-    public int[] findWorstSubIntWithBruteforceAlgorithm(int[] interval){
+    public int[] findWorstSubIntWithBruteforceAlgorithm(Date firstDayToAnalyse, Date lastDayToAnalyse){
+        int[] interval = makeIntervalArray(firstDayToAnalyse, lastDayToAnalyse);
         //TO DO
         return null;
     }
