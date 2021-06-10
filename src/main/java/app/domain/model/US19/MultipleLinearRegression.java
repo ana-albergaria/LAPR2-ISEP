bd74b623e6b2f018ea1f5ed7cad5742ccc9aca53
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-10 16:30:21 +0100

    US19 Multiple Linear Regression Class Continuation

--- src/main/java/app/domain/model/US19/MultipleLinearRegression.java
+++ src/main/java/app/domain/model/US19/MultipleLinearRegression.java
@@ -16,9 +16,44 @@ public class MultipleLinearRegression {
         //determine X
         double[][] x = new double[x1.length][3];
         for (int i = 0; i < x.length; i++) {
-
-
+            x[i][0] = 1;
+            x[i][1] = x1[i];
+            x[i][2] = x2[i];
         }
 
+        System.out.println("X:");
+        imprimir(x);
+        System.out.println();
+
+        //determine xT (transpose of the matrix X)
+        int rowX = x.length, columnX = x[0].length;
+        double[][] xT = new double[columnX][rowX];
+        for (int i = 0; i < rowX; i++) {
+            for (int j = 0; j < columnX; j++) {
+                xT[j][i] = x[i][j];
+            }
+        }
+
+        System.out.println("xT:");
+        imprimir(xT);
+        System.out.println();
+
+
+
+
+    }
+    /*
+    private double[][] matrixMultiplication(double[][] matrix, double[][] otherMatrix) {
+
+    }
+     */
+
+    public static void imprimir(double[][] array) {
+        for (int linha = 0; linha < array.length; linha++) {
+            for (int coluna = 0; coluna < array[linha].length; coluna++) {
+                System.out.print(array[linha][coluna] + " ");
+            }
+            System.out.println();
+        }
     }
 }
