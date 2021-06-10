5041da46346907e727318a5b178cf96806d19203
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-10 21:23:35 +0100

    US19 Multiple Linear Regression Class Continuation

--- src/main/java/app/domain/model/US19/MultipleLinearRegression.java
+++ src/main/java/app/domain/model/US19/MultipleLinearRegression.java
@@ -38,15 +38,125 @@ public class MultipleLinearRegression {
         imprimir(xT);
         System.out.println();
 
+        //determine xTx
+
+        double[][] xTx = matrixMultiplication(xT, x);
+
+        System.out.println("xTx:");
+        imprimir(xTx);
+        System.out.println();
+
+        double[][] xTxInverse = invert(xTx);
+
+        System.out.println("xTxInverse: ");
+        imprimir(xTxInverse);
+        System.out.println();
+
+        //double[][] xTy = matrixMultiplication(xT, y);
+
+
+
+
 
 
 
     }
-    /*
+
     private double[][] matrixMultiplication(double[][] matrix, double[][] otherMatrix) {
+        if(matrix[0].length != otherMatrix.length)
+            throw new IllegalArgumentException("The multiplication is not possible with" + matrix[0].length + "column from the" +
+                    "first matrix and" + otherMatrix.length + "lines from the second matrix!");
 
+        double[][] result = new double[matrix.length][otherMatrix[0].length];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < otherMatrix[0].length; column++) {
+                for (int k = 0; k < matrix[0].length; k++) {
+                    result[row][column] += matrix[row][k] * otherMatrix[k][column];
+                }
+            }
+        }
+        return result;
     }
-     */
+
+    private double[][] invert(double a[][]) {
+        int n = a.length;
+        double x[][] = new double[n][n];
+        double b[][] = new double[n][n];
+        int index[] = new int[n];
+        for (int i=0; i<n; ++i)
+            b[i][i] = 1;
+
+        // Transform the matrix into an upper triangle
+        gaussian(a, index);
+
+        // Update the matrix b[i][j] with the ratios stored
+        for (int i=0; i<n-1; ++i)
+            for (int j=i+1; j<n; ++j)
+                for (int k=0; k<n; ++k)
+                    b[index[j]][k] -= a[index[j]][i] * b[index[i]][k];
+
+        // Perform backward substitutions
+        for (int i=0; i<n; ++i) {
+            x[n-1][i] = b[index[n-1]][i] / a[index[n-1]][n-1];
+            for (int j=n-2; j>=0; --j) {
+                x[j][i] = b[index[j]][i];
+                for (int k=j+1; k<n; ++k) {
+                    x[j][i] -= a[index[j]][k] * x[k][i];
+                }
+                x[j][i] /= a[index[j]][j];
+            }
+        }
+        return x;
+    }
+
+    // Method to carry out the partial-pivoting Gaussian
+    // elimination.  Here index[] stores pivoting order.
+    private void gaussian(double[][] a, int[] index) {
+        int n = index.length;
+        double c[] = new double[n];
+
+        // Initialize the index
+        for (int i = 0; i < n; ++i)
+            index[i] = i;
+
+        // Find the rescaling factors, one from each row
+        for (int i=0; i<n; ++i) {
+            double c1 = 0;
+            for (int j=0; j<n; ++j) {
+                double c0 = Math.abs(a[i][j]);
+                if (c0 > c1) c1 = c0;
+            }
+            c[i] = c1;
+        }
+
+        // Search the pivoting element from each column
+        int k = 0;
+        for (int j=0; j<n-1; ++j) {
+            double pi1 = 0;
+            for (int i=j; i<n; ++i) {
+                double pi0 = Math.abs(a[index[i]][j]);
+                pi0 /= c[index[i]];
+                if (pi0 > pi1) {
+                    pi1 = pi0;
+                    k = i;
+                }
+            }
+
+            // Interchange rows according to the pivoting order
+            int itmp = index[j];
+            index[j] = index[k];
+            index[k] = itmp;
+            for (int i=j+1; i<n; ++i) {
+                double pj = a[index[i]][j] / a[index[j]][j];
+                // Record pivoting ratios below the diagonal
+                a[index[i]][j] = pj;
+                // Modify other elements accordingly
+                for (int l=j+1; l<n; ++l)
+                    a[index[i]][l] -= pj * a[index[j]][l];
+            }
+        }
+    }
 
     public static void imprimir(double[][] array) {
         for (int linha = 0; linha < array.length; linha++) {
