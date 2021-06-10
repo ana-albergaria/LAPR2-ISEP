94572b97f65f6551d49fb17a72deb7705029b7ea
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-10 15:27:47 +0100

    US19 Starting to do Multiple Linear Regression Class

--- src/main/java/app/domain/model/US19/MultipleLinearRegression.java
+++ src/main/java/app/domain/model/US19/MultipleLinearRegression.java
@@ -1,4 +1,24 @@
 package app.domain.model.US19;
 
 public class MultipleLinearRegression {
+    private double[] xTx;
+    private double[] xTxInverse;
+    private double[] xTy;
+    private double[] regressionCoefficients;
+    private int n;
+
+    public MultipleLinearRegression(double[] x1, double[] x2, double[] y) {
+        if (x1.length != x2.length || x1.length != y.length) {
+            throw new IllegalArgumentException("Array lengths are not equal!");
+        }
+        n = x1.length;
+
+        //determine X
+        double[][] x = new double[x1.length][3];
+        for (int i = 0; i < x.length; i++) {
+
+
+        }
+
+    }
 }
