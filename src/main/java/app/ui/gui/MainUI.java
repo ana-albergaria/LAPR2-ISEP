a6f6657e9f296f3eee6784cfbe2e27a116ccecbe
martaribeiro <1201592@isep.ipp.pt>
2021-06-11 10:27:01 +0100

    Added javaFX to project.

---
+++ src/main/java/app/ui/gui/MainUI.java
@@ -0,0 +1,17 @@
+package app.ui.gui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainUI implements Initializable {
+
+    /**
+     * Initializes the UI class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
