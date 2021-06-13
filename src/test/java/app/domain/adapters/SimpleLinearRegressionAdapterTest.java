29904edb7c18e8ca8416012f2b0035ea52498c81
Ana Albergaria <1201518@isep.ipp.pt>
2021-06-13 22:14:56 +0100

    US19 Testing MyRegressionModel Class

--- src/test/java/app/domain/adapters/SimpleLinearRegressionAdapterTest.java
+++ src/test/java/app/domain/adapters/SimpleLinearRegressionAdapterTest.java
@@ -10,7 +10,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SimpleSimpleLinearRegressionAdapterTest {
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleLinearRegressionAdapterTest {
     private Company company;
     private RegressionModel regressionModel;
     private MyRegressionModel myRegressionModel;
@@ -37,8 +41,39 @@ public class SimpleSimpleLinearRegressionAdapterTest {
     public void getEstimatedPositives() {
         //Ex. 1 TP PL7 MATCP
         double[] x = {825.0, 215.0, 1070.0, 550.0, 480.0, 920.0, 1350.0, 325.0, 670.0, 1215.0};
+        double[] y = {3.5, 1.0, 4.0, 2.0, 1.0, 3.0, 4.5, 1.5, 3.0, 5.0};
+        Double[] xInDouble = {825.0, 215.0, 1070.0, 550.0, 480.0, 920.0, 1350.0, 325.0, 670.0, 1215.0};
+        Double[] yInDouble = {3.5, 1.0, 4.0, 2.0, 1.0, 3.0, 4.5, 1.5, 3.0, 5.0};
+
+        List<String> expectedEstimatedPositives = new ArrayList<>();
+        expectedEstimatedPositives.add("3,075863");
+        expectedEstimatedPositives.add("0,888933");
+        expectedEstimatedPositives.add("3,954221");
+        expectedEstimatedPositives.add("2,089952");
+        expectedEstimatedPositives.add("1,838993");
+        expectedEstimatedPositives.add("3,416451");
+        expectedEstimatedPositives.add("4,958058");
+        expectedEstimatedPositives.add("1,283297");
+        expectedEstimatedPositives.add("2,520168");
+        expectedEstimatedPositives.add("4,474065");
 
 
+        MyRegressionModel myRegressionModel = regressionModel.getRegressionModel(x, null, y, 10);
+        List<Double> estimatedPositives = regressionModel.getEstimatedPositives(myRegressionModel, xInDouble, yInDouble);
+
+        /*
+         * it was necessary to convert to an Array of String because JUNIT doesn't
+         * have an Assert.assertEquals with a delta for Double Arrays
+         */
+
+        List<String> estimatedPositivesString = new ArrayList<>();
+        DecimalFormat df = new DecimalFormat("#.######");
+
+        for (Double estimatedPositive : estimatedPositives) {
+            estimatedPositivesString.add(df.format(estimatedPositive));
+        }
+
+        Assert.assertEquals(expectedEstimatedPositives, estimatedPositivesString);
     }
 
     @Test
