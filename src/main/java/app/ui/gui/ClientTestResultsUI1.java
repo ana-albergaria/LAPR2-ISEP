1d06eb0a89984eade4d6d7033bfc5e165ca8a515
martaribeiro <1201592@isep.ipp.pt>
2021-06-14 17:34:10 +0100

    Continued US1 UI.

--- src/main/java/app/ui/gui/ClientTestResultsUI1.java
+++ src/main/java/app/ui/gui/ClientTestResultsUI1.java
@@ -10,18 +10,30 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClientTestResultsUI1 implements Initializable {
 
     private App mainApp;
+
+    private final String FXML_PATH = "/fxml/ClientTestResults1.fxml";
+
+    public App getMainApp() {
+        return this.mainApp;
+    }
+
+    public void setMainApp(App mainApp){
+        this.mainApp=mainApp;
+    }
+
     private ClientMenuUI clientMenuUI;
     private ViewClientResultsController controller;
 
@@ -45,22 +57,40 @@ public class ClientTestResultsUI1 implements Initializable {
 
     @FXML
     void exitAction(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alert.setTitle("Application");
+        alert.setHeaderText("Action confirmation.");
+        alert.setContentText("Do you really wish to exit the application?");
+
+        ((Labeled) alert.getDialogPane().lookupButton(ButtonType.OK)).setText("Yes");
+        ((Labeled) alert.getDialogPane().lookupButton(ButtonType.CANCEL)).setText("No");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            ((Stage) exitBtn.getScene().getWindow()).close();
+        }
     }
 
     @FXML
     void returnAction(ActionEvent event) {
+        try {
+            ClientMenuUI clientMenuUI = (ClientMenuUI) this.mainApp.replaceSceneContent("/fxml/ClientMenu.fxml");
+            clientMenuUI.setMainApp(mainApp);
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        List<TestDTO> testsWithResults = controller.getClientTestsWithResults(this.clientMenuUI.getMainUI().getEmail());
+        /*List<TestDTO> testsWithResults = controller.getClientTestsWithResults(this.clientMenuUI.getMainUI().getEmail());
         updateClientTestsInfoForTableview(testsWithResults, clientTestsInfoForTableview);
         colTestType.setCellValueFactory(new PropertyValueFactory<ClientTestsInfoForTableview,String>("testType"));
         colDateOfTestRegistration.setCellValueFactory(new PropertyValueFactory<ClientTestsInfoForTableview,String>("stringDateOfTestRegistration"));
-        tableView.setItems(clientTestsInfoForTableview);
+        tableView.setItems(clientTestsInfoForTableview);*/
     }
 
-    private ObservableList<ClientTestsInfoForTableview> clientTestsInfoForTableview = FXCollections.observableArrayList();
+    /*private ObservableList<ClientTestsInfoForTableview> clientTestsInfoForTableview = FXCollections.observableArrayList();
 
     public void updateClientTestsInfoForTableview(List<TestDTO> testsToAdd, List<ClientTestsInfoForTableview> clientTestsInfoForTableview){
         String description, date;
@@ -71,6 +101,6 @@ public class ClientTestResultsUI1 implements Initializable {
             toAdd = new ClientTestsInfoForTableview(description,date);
             clientTestsInfoForTableview.add(toAdd);
         }
-    }
+    }*/
 
 }
